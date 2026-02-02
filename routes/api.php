751eee2e5dc6de9d8a23cb3cdd55dcf39c6ce899
valiant77 <achmadfahri0721@gmail.com>
2026-02-02 10:57:57 +0700

    API #2

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\AbsenController;
+use App\Http\Controllers\Api\AbsenController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
